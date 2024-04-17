615ec4022a8eaf2c99556f25679a1bb56a7bee02
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-17 08:50:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	var result = "";
+	var char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for (var i = 0; i<l; i++)
+		{
+			res+= char_list.charAt(math.floor(math.random()*char_list.length))
+		}
+return res;	
 }
 
 // Do not change the code below.
